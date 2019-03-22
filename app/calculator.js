6fd6c8c883801a7347c0b2a47544891647b027fe
Portia Patterson <portialorrainepatterson@gmail.com>
2019-03-22 10:28:26 +0000

    Files I worked on during day 1

--- app/calculator.js
+++ app/calculator.js
@@ -2,6 +2,40 @@ function add(a, b) {
     return a + b;
 }
 
+
+
+function subtract(x, y) {
+    return x-y;
+}
+    
+
+function multiply(c, d) {
+    return c * d;
+}
+    
+
+function isOdd(e) {
+ if (e % 2 == 1)
+ return true
+ else
+ return false
+}
+
+function divisibleBy(g, h) {
+    if (g % h == 0)
+    return true
+    else
+    return false
+   }
+
+   function checkNumberSize(i, j) {
+    if (i > j)
+    return  i + " is bigger than" + j
+    else
+    return  j + " is bigger than" + i
+   }  
+
 module.exports = {
-    add
+    subtract, add, multiply, isOdd, divisibleBy, checkNumberSize
 };
+
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -3,3 +3,22 @@ const calculator = require('../app/calculator');
 test('adds 1 + 2 to equal 3', () => {
   expect(calculator.add(1, 2)).toBe(3);
 });
+
+test('subtracts 3 - 2 to equal 1', () => {
+  expect(calculator.subtract(3, 2)).toBe(1);
+});
+
+test('multiply 3 * 2 to equal 6', () => {
+  expect(calculator.multiply(3, 2)).toBe(6);
+});
+
+test('isOdd 3 % 2 to equal 1', () => {
+  expect(calculator.isOdd(3)).toBe(true);
+});
+
+test('divisibleBy 4 % 2 has no remainder', () => {
+  expect(calculator.divisibleBy(4, 2)).toBe(true);
+});
+test('checkNumberSize 10 > 2 should show 10 is bigger than 2', () => {
+  expect(calculator.divisibleBy(4, 2)).toBe(true);
+});
